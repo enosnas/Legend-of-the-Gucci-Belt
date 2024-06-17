f3034c0e8e11f8fcb4a72babb22421aabb906a50
enosnas <rileysletsplay@gmail.com>
2024-06-17 12:32:08 -0700

    dialogue package first commit
    Adding dialogue package and begin editing levels with new boss arenas

---
+++ Assets/Plugins/Pixel Crushers/Dialogue System/Scripts/Options/Timeline/Playables/RunLua/RunLuaClip.cs
@@ -0,0 +1,31 @@
+// Recompile at 6/17/2024 12:29:14 PM
+#if USE_TIMELINE
+#if UNITY_2017_1_OR_NEWER
+// Copyright (c) Pixel Crushers. All rights reserved.
+
+using System;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+namespace PixelCrushers.DialogueSystem
+{
+    [Serializable]
+    public class RunLuaClip : PlayableAsset, ITimelineClipAsset
+    {
+        public RunLuaBehaviour template = new RunLuaBehaviour();
+
+        public ClipCaps clipCaps
+        {
+            get { return ClipCaps.None; }
+        }
+
+        public override Playable CreatePlayable(PlayableGraph graph, GameObject owner)
+        {
+            var playable = ScriptPlayable<RunLuaBehaviour>.Create(graph, template);
+            return playable;
+        }
+    }
+}
+#endif
+#endif
